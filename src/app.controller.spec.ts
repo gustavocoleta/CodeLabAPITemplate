2b42ebffbba374aeab6d5f55852c63f901dc20b0
gustavocoleta <gustavo.coleta@vrsoft.com.br>
2024-04-08 22:03:30 -0300

    Aula 6

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,12 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello CodeLab!"', () => {
+      expect(appController.getHello()).toBe('Hello CodeLab!');
+    });
+
+    it('should return <> "Hello CodeLab!"', () => {
+      expect(appController.getHello()).not.toBe('ABC');
     });
   });
 });
